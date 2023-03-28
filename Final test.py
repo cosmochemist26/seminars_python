dc99332eb6cc676ad108af71c870779020186b54
Lyubov <love.rodionova.spb@gmail.com>
2023-03-28 20:55:29 +0300

    test added

--- Final test.py
+++ Final test.py
@@ -15,6 +15,22 @@
 --- '\u0440\u0430\u0437\u0440\u0430\u0431\u043e\u0442\u043a\u0430' - набор кодовых точек
 --- используйте списки и циклы, не дублируйте функции
 """
+words = ['разработка', 'сокет', 'декоратор']
+
+for line in words:
+    print('тип переменной: {}\n'.format(type(line)))
+    print('содержание переменной - {}\n'.format(line))
+    print('длина строки: {}\n'.format(len(line)))
+
+
+words = [b'\xd1\x80\xd0\xb0\xd0\xb7\xd1\x80\xd0\xb0\xd0\xb1\xd0\xbe\xd1\x82\xd0\xba\xd0\xb0',
+       b'\xd1\x81\xd0\xbe\xd0\xba\xd0\xb5\xd1\x82',
+       b'\xd0\xb4\xd0\xb5\xd0\xba\xd0\xbe\xd1\x80\xd0\xb0\xd1\x82\xd0\xbe\xd1\x80']
+
+for line in words:
+    print('тип переменной: {}\n'.format(type(line)))
+    print('содержание переменной - {}\n'.format(line))
+    print('длина строки: {}\n'.format(len(line)))
 
 """
 Задание 2.
@@ -28,9 +44,12 @@
 --- b'class' - используйте маркировку b''
 --- используйте списки и циклы, не дублируйте функции
 """
+words = [b'class', b'function', b'method']
 
-
-
+for line in words:
+    print('тип переменной: {}\n'.format(type(line)))
+    print('содержание переменной - {}\n'.format(line))
+    print('длинна строки: {}\n'.format(len(line)))
 
 """
 Задание 3.
@@ -44,13 +63,19 @@
 придумайте как это сделать
 """
 
+"""
+word1 = b'attribute'
+word2 = b'класс'
+word3 = b'функция'
+word4 = b'type'
 
+#на строки, написанные на кириллице, выдается исключение
 
-
-
-
-
-
+File "C:\Users\Lyubov\PycharmProjects\pythonProject\seminars_python\Final test.py", line 2
+    word2 = b'класс'
+          ^
+SyntaxError: bytes can only contain ASCII literal characters.'''
+"""
 
 """
 Задание 4.
@@ -61,16 +86,16 @@
 
 Подсказки:
 --- используйте списки и циклы, не дублируйте функции
+
+words = ['разработка', 'администрирование', 'protocol', 'standard']
+
+for word in words:
+    b_word = word.encode('utf-8')
+    print(f'{word} в байтовом представлении: {b_word}')
+    decoded_word = b_word.decode('utf-8')
+    print(f'{b_word} в строковом представлении: {decoded_word}')
+
 """
-
-
-
-
-
-
-
-
-
 
 """
 Задание 5.
@@ -80,3 +105,25 @@
 
 Подсказки:
 --- используйте модуль chardet, иначе задание не засчитается!!!
+""" 
+
+import subprocess
+import chardet
+
+class Ping:
+    def __init__(self, urls):
+        self.urls = urls
+
+    def run_ping(self):
+        for url in self.urls:
+            process = subprocess.Popen(['ping', '-c', '4', url],stdout=subprocess.PIPE)
+            output, error = process.communicate()
+            encoding = chardet.detect(output)['encoding']
+            decoded_output = output.decode(encoding)
+            print(f'Ping для {url}:')
+            print(decoded_output)
+
+
+urls = ['yandex.ru', 'youtube.com']
+ping = Ping(urls)
+ping.run_ping()
